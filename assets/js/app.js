7eca0e55c3bb184c3f6e2880163f8aaa041adf93
joe <burnett.joe@gmail.com>
2018-09-12 12:14:48 -0400

    adding html elements for bare layout

---
+++ assets/js/app.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyBTEFaP_4m6vdnCYyT9HgUcGB-oysLJljI",
+    authDomain: "train-schedule-30661.firebaseapp.com",
+    databaseURL: "https://train-schedule-30661.firebaseio.com",
+    projectId: "train-schedule-30661",
+    storageBucket: "train-schedule-30661.appspot.com",
+    messagingSenderId: "232813586151"
+};
+firebase.initializeApp(config);
